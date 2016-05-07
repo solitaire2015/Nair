c2cfb65d13c5a1c3a98b70a052bf80e9fad16d92
Craig.C.Li <Craig.C.Li@newegg.com>
2016-05-07 15:34:23 +0800

    client implement

---
+++ Tairclient/TairLib/TairClient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thrift.Collections;
+using Thrift.Protocol;
+using Thrift.Transport;
+
+namespace TairLib
+{
+    public class TairClient
+    {
+        private TSocket socket;
+        private TFramedTransport transport;
+        private TProtocol protocol;
+        private tair_serv.Client client;
+        public TairClient()
+        {
+            socket = new TSocket("192.168.112.128", 9090);
+            transport = new TFramedTransport(socket);
+            protocol = new TBinaryProtocol(transport);
+            client = new tair_serv.Client(protocol);
+            transport.Open();
+        }
+        public string Get(long area,string key)
+        {
+            return client.Tair_get(area, key);
+        }
+        public long Put(long area,string key,string value,long expire,long version)
+        {
+            return client.Tair_put(area, key, value, expire, version);
+        }
+        public bool Begin(string master_addr,string slave_addr,string groupName)
+        {
+            return client.Tair_begin(master_addr, slave_addr, groupName);
+        }
+        public void SetLogLevel(string level)
+        {
+            client.Tair_set_loglevel(level);
+        }
+        public long Remove(long area,string key)
+        {
+            return client.Tair_remove(area, key);
+        }
+    }
+}
---
+++ Tairclient/tairtest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TairLib;
+
+namespace tairtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TairClient client = new TairClient();
+            client.Begin("192.168.112.128:5198", "192.168.112.128:5198", "group_1");
+            long re = client.Put(0, "lichao", "love", 0, 0);
+            string look = client.Get(0, "lichao");
+            client.Remove(0, "lichao");
+            string look1 = client.Get(0, "lichao");
+            int i = 1;
+        }
+    }
+}
